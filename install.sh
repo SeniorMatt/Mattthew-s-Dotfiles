234b0d47b0cac52fd0c0d2c3bde318460d760b60
Senior Matthew <matthew.prakhov@gmail.com>
2025-05-19 19:56:05 +0500

    0.0.8

--- install.sh
+++ install.sh
@@ -1,72 +1,78 @@
 #!/bin/bash
 
-apps=(
-  firefox
-  nvim
-  discord
-  telegram-desktop
-  obsidian
-  github-desktop-bin
-  qbittorrent
+# List of packages
+applications=(
+  firefox  # Browser
+  nvim     # Text editor
+  obsidian # Note taking app
   steam
   zoom
   aseprite
+  github-desktop-bin
+  telegram-desktop
+  discord
+  qbittorrent
 )
 
-# List of packages
+default_applications=(
+  nautilus             # File manager
+  eog                  # Image viewer
+  evince               # Document viewer
+  decibels             # Media player
+  baobab               # Disk analyzer
+  gnome-software       # Browse apps
+  blueberry            # Bluetooth control
+  nm-connection-editor # WiFi control
+  pavucontrol          # Audio control
+  kooha                # Video record
+  ffmpeg               # Convertor
+  htop                 # Process viewer
+)
+
+additional_packages=(
+  xwaylandvideobridge   # Screen sharing
+  sof-firmware          # Sound for Intel latest CPUs
+  cups                  # Printers
+  pipewire              # Sound server
+  power-profiles-daemon # Power profiles switcher
+  zoxide                # Makes moving in terminal much easier
+  yay                   # AUR
+  python-pywal16        # Color generator
+)
+
 packages=(
-  # Packages
-  xwaylandvideobridge
-  # sof-firmware
-  cups
-  pipewire
-  zoxide
-  yay
+  hyprland                    # WM
+  ly                          # LM
+  kitty                       # Terminal
+  waybar                      # Status waybar
+  tofi                        # App launcher
+  waypaper                    # GUI Wallpaper manager
+  hyprpaper                   # Wallpaper backend
+  swaync                      # Notifications
+  wl-clipboard                # Clipboard manager
+  hyprlock                    # Lock manager
+  hypridle                    # Idle manager
+  wlogout                     # Logout menu
+  nwg-look                    # GTK Themer
+  udiskie                     # Removable media
+  hyprsunset                  # Blue light filter
+  hyprpicker                  # Color picker
+  hyprpolkitagent             # Authentication daemon
+  xdg-desktop-portal-hyprland # Screen sharing Hyprland
+)
 
-  # System Packages
-  hyprland
-  ly
-  kitty
-  waybar
-  rofi-lbonn-wayland-git
-  rofi-calc
-  rofi-emoji
-  waypaper
-  swaync
-  hyprpaper
-  hyrpsunset
-  xdg-desktop-portal-hyprland
-  wl-clipboard
-  hyprlock
-  udiskie
-  wlogout
-  python-pywal16
-  # betterdiscord-installer
-  nwg-look
+fonts=(
+  ttf-jetbrains-mono     # System font
+  noto-fonts-emoji       # Emoji font
+  awesome-terminal-fonts # Better align icons
+)
 
-  # Useful Utilities
-  nautilus
-  baobab
-  eog
-  evince
-  decibels
-  blueberry
-  pavucontrol
-  nm-connection-editor
-  kooha
-  htop
-  power-profiles-daemon
+themes=(
+  bibata-cursor-theme # Cursor theme
+  adw-gtk-theme       # GTK3 theme
+)
 
-  # Themes
-  bibata-cursor-theme
-  adw-gtk-theme
-
-  # Fonts
-  ttf-jetbrains-mono
-  awesome-terminal-fonts
-  otf-font-awesome
-
-  # Terminal Stuff
+terminal_packages=(
   neofetch
   cmatrix
   cava
@@ -77,10 +83,59 @@ packages=(
   cbonsai
 )
 
+# Update the systemc
 sudo pacman -Sy --noconfirm
 
 # Install packages
+for pkg in "${applications[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${default_applications[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${additional_packages[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
 for pkg in "${packages[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${fonts[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${themes[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${terminal_packages[@]}"; do
   echo "Installing $pkg..."
   if ! pacman -Qq "$pkg" &>/dev/null; then
     yay -S --noconfirm --needed "$pkg"
